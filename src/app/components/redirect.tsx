a81aa3f694270fb769f91a56d06a01ee216c6cab
Rupesh Karale <rupeshkarale3245@gmail.com>
2024-11-14 16:13:46 +0530

    check without timeout

--- src/app/components/redirect.tsx
+++ src/app/components/redirect.tsx
@@ -12,16 +12,14 @@ const RedirectOnMount = ({ targetUrl }: { targetUrl?: string }) => {
   //   }, [router, targetUrl]);
 
   useEffect(() => {
-    setTimeout(() => {
-      const pathSegments = pathname.split("/");
-      const id = pathSegments[pathSegments.length - 1];
+    // setTimeout(() => {
+    const pathSegments = pathname.split("/");
+    const id = pathSegments[pathSegments.length - 1];
 
-      //   router.push(`https://testing.eventy.xyz/e/title/${targetUrl}`);
-      if (id)
-        router.push(
-          `https://testing.eventy.xyz/event/testing-11-nov-2024/${id}`
-        );
-    }, 1000);
+    //   router.push(`https://testing.eventy.xyz/e/title/${targetUrl}`);
+    if (id)
+      router.push(`https://testing.eventy.xyz/event/testing-11-nov-2024/${id}`);
+    // }, 1000);
   }, [router, targetUrl]);
 
   return null; // No need to render anything
